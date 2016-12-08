5c29a200fef913c6afc32c10a99c1fcd732ec4a9
Kalvin <khanhqd.neu@gmail.com>
2016-12-08 14:16:45 +0700

    no message

--- firstproject/index.android.js
+++ firstproject/index.android.js
@@ -1,53 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  Platform,
+  Dimensions,
+  Button
 } from 'react-native';
 
+var {height, width} = Dimensions.get('window');
 export default class firstproject extends Component {
+
+  onButtonPress(){
+    Alert.alert('pressed', 'Edit button pressed');
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={{flex: 1, paddingTop:20 }}>
+        <View style={{flexDirection:'row', width:width , justifyContent: 'space-around'}}>
+          <Button
+            onPress={this.onButtonPress.bind()}
+            title="Ngày"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+            onPress={this.onButtonPress.bind()}
+            title="Tuần"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+            onPress={this.onButtonPress.bind()}
+            title="Tháng"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+        <View style={{width:width-30,margin:15, borderWidth:2, borderRadius: 2}}>
+          <View>
+            <Text style={styles.text}>Tổng số cuộc gọi</Text>
+          </View>
+            <View style={styles.box}>
+              <Text>15/30</Text>
+            </View>
+            <View>
+              <Text style={styles.text}>Tổng số đặt hẹn</Text>
+            </View>
+
+              <View style={styles.box}>
+                <Text>15/30</Text>
+              </View>
+              <View>
+                <Text style={styles.text}>Tổng số mua hàng</Text>
+              </View>
+
+                <View style={styles.box}>
+                  <Text>15/30</Text>
+                </View>
+        </View>
+
+
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
+  button: {
     textAlign: 'center',
-    margin: 10,
+    fontSize: 18,
+    marginBottom: 10,
+    marginTop: 10,
+    color: 'blue'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  box:{
+    padding : 5,
+    borderWidth : 2,
+    borderRadius: 2
   },
+  text:{
+    padding: 10,
+    fontSize: 14
+  }
 });
+
 
 AppRegistry.registerComponent('firstproject', () => firstproject);
